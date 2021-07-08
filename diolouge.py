04eb19cabaf0028e44948327a982228af079be5b
Itthomas <isaaclthomas@gmail.com>
2021-07-08 02:56:53 -0600

    Added multiple choice dialogue, game logic, and some levels

---
+++ diolouge.py
@@ -0,0 +1,21 @@
+import arcade
+import random
+from globalVars import *
+
+class Diolouge:
+    def __init__(self, level):
+        self.level = level
+        self.multiplier = random.randint(0,9)
+        self.answer = self.level * self.multiplier
+        valid = False
+        self.correct_index = random.randint(0, 3)
+        self.multiple_choice = [-1, -1, -1, -1]
+        while valid == False:
+            self.multiple_choice = [-1, -1, -1, -1]
+            self.multiple_choice[self.correct_index] = self.answer
+            for i in range(4):
+                if self.multiple_choice[i] == -1:
+                    self.multiple_choice[i] = random.randint(0, 99)
+            if len(set(self.multiple_choice)) == len(self.multiple_choice):
+                valid = True
+        
--- globalVars.py
+++ globalVars.py
@@ -2,11 +2,11 @@ SCREEN_WIDTH = 1300
 SCREEN_HEIGHT = 750
 SCREEN_TITLE = "Adventure in Multiplication Land (TM)"
 GRAVITY = -1
-FRICTION = 1.12
-DRAG = 2
-PLAYER_JUMP_SPEED =22
+FRICTION = 1.1
+DRAG = 1.023
+PLAYER_JUMP_SPEED =23
 PLAYER_MOVEMENT_SPEED = 0.8
-PLAYER_MAX_MOVEMENT_SPEED = 13
+PLAYER_MAX_MOVEMENT_SPEED = 12
 BACKGROUND_WIDTH = 2000
 
-level_templates = ['Images/map0.png', 'Images/map1.png']
+level_templates = ['Images/map2.png', 'Images/map1.png', 'Images/map2.png', 'Images/map3.png']
