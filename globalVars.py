bcba7dc8d94c1245fbb065dd372f1831f7aa08ca
Jesse <79804684+jmp1025@users.noreply.github.com>
2021-07-08 09:34:53 -0700

    Player2 update

--- globalVars.py
+++ globalVars.py
@@ -7,6 +7,10 @@ DRAG = 1.023
 PLAYER_JUMP_SPEED = 23
 PLAYER_MOVEMENT_SPEED = 0.8
 PLAYER_MAX_MOVEMENT_SPEED = 12
+PLAYER_FACE_LEFT = 1
+PLAYER_FACE_RIGHT = 0
+PLAYER_UPDATE = 0.8
+PLAYER_SCALE = 1
 BACKGROUND_WIDTH = 2000
 
 level_templates = ['Images/map0.png', 'Images/map1.png', 'Images/map2.png', 'Images/map3.png']
